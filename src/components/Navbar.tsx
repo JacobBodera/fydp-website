82bc6a39d67077f59afa4d51ddc19afa87a892cc
Jacob Bodera <jacobbodera@Jacobs-MacBook-Air.local>
2024-11-25 14:42:05 -0500

    Added project bame

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ export const Navbar = () => {
     <div className="w-full max-h-min flex flex-col top-0 bg-transparent p-4">
       <div className="text-6xl font-bold mb-2">Capstone Design Project</div>
       <div className="text-xl font-bold text-gray tracking-[12px]">
-        FocusFind - Keeping your world in focus
+        Recall - Keeping your world in focus
       </div>
     </div>
   );
